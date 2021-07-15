676aa97ebccb0d4a62882026fbafe3ba151f14f7
niklas3000 <niklas3000@gmail.com>
2021-07-15 21:05:52 +0200

    lagt till val för användaren

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,22 +8,41 @@ import streamlit as st
 Interaktiv graf för opinionsundersökningar
 """
 
+partier = ['V', 'S', 'MP','C', 'L', 'M', 'KD', 'SD']
+färger = ['darkred', 'red', 'darkgreen', 'green', 'deepskyblue', 'blue', 'darkblue', 'yellow']
+
+close_to_cut_off = st.checkbox("Visa endast partier som ligger nära spärren", value=True)
+visa_sista_måndaerna = st.checkbox("Visa bara sista måndaderna", value=True)
+Visa_snitt = st.checkbox("visa snittvärden", value=True)
+
 df = pd.read_csv("polls_edit.csv", delimiter=';')
 df['Datum'] = pd.to_datetime(df.PublYearMonth)
 
+if close_to_cut_off:
+    valda = df.head(1)[partier]<6.15
+    from itertools import compress
+    partier = list(compress(partier, valda.to_numpy().tolist()[0]))
+    färger = list(compress(färger, valda.to_numpy().tolist()[0]))
 
+if visa_sista_måndaerna:
+    df = df[df['Datum'] > '2021-04-01']
+
+if visa_snitt:
+    uttryck = 'mean(stöd):Q'
+else:
+    uttryck = 'stöd:Q'
 
 st.altair_chart(
     alt.Chart(df).mark_line().transform_fold(
-        fold=['V', 'S', 'MP','C', 'L', 'M', 'KD', 'SD'], 
+        fold=partier, 
         as_=['parti', 'stöd']
     ).encode(
         x='Datum',
-        y = alt.Y('mean(stöd):Q', title="Procent"),
+        y = alt.Y(uttryck, title="Procent"),
 
         color = alt.Color('parti:N', 
-            scale=alt.Scale(domain = ['V', 'S', 'MP','C', 'L', 'M', 'KD', 'SD'], 
-            range=['darkred', 'red', 'green', 'darkgreen', 'deepskyblue', 'blue', 'darkblue', 'yellow'])),
-        tooltip = ['parti:N', 'mean(stöd):Q']
+            scale=alt.Scale(domain = partier, 
+            range=färger)),
+        tooltip = ['parti:N', uttryck]
     ).interactive()
 )
