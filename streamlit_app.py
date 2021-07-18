815e27e66c31bc15719559d432392bc19bde5187
niklas3000 <niklas3000@gmail.com>
2021-07-18 21:52:45 +0200

    ändrat text på knappar

--- streamlit_app.py
+++ streamlit_app.py
@@ -149,7 +149,7 @@ st.set_page_config(
 """
 
 # grunddata navigering
-användar_val = ['de små partierna', 'de större partierna', 'de blocken']
+användar_val = ['de små partierna', 'de större partierna', 'de två blocken']
 
 
 # navigering
