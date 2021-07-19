4a4f8faa462ff572660bf2c13a03b4183b5c81f1
niklas3000 <niklas3000@gmail.com>
2021-07-19 12:14:50 +0200

    lagt till siffervisning för block, refaktoriserat koden, uppdaterat text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,27 @@
 import altair as alt
 from altair.vegalite.v4.api import Chart, value
+from altair.vegalite.v4.schema.channels import Tooltip
 from altair.vegalite.v4.schema.core import Padding
 import pandas as pd
 import streamlit as st
 from PIL import Image
 from itertools import compress
 
+class GrundData:
+    block = ['Regering + stöd', 'Regering + stöd', 'Regering + stöd', 'Regering + stöd', 'Högeropposition', 'Högeropposition', 'Högeropposition', 'Högeropposition']
+    färger = ['darkred', 'red', 'darkgreen', 'green', 'deepskyblue', 'blue', 'darkblue', 'yellow']
+    partier = ['V', 'S', 'MP','C', 'L', 'M', 'KD', 'SD']
+    gräns_småparti = 6.0
+    riksdagsspärr = 4.0
 
 class OpinionChart:
-    def __init__(self):
-        self.titel = ""
-        self.df = OpinionChart.läs_och_preparera_tidsserie_data()
-        self.partier_urval = None
-        self.färger_urval = None
-        self.datum_utttryck = None
-        self.y_uttryck = None
-        self.tool_uttryck = None
-        self.små_partier_bool = None
-        self.chart = None
 
 
     @staticmethod
-    def läs_och_preparera_tidsserie_data():
+    def läs_och_preparera_tidsserie_data(tidsserie: bool = True):
         df = pd.read_csv("polls_edit.csv", delimiter=';')
+        if tidsserie == False:
+            return df
         df['Publiceringsdatum'] = pd.to_datetime(df.PublDate)
         df.rename(columns={'Company': 'Institut'}, inplace=True)
         df = df[df['Publiceringsdatum'] > '2021-02-01']
@@ -31,54 +30,49 @@ class OpinionChart:
 
     @staticmethod
     def skapa_block_chart():
-        block = ['Regering + stöd', 'Regering + stöd', 'Regering + stöd', 'Regering + stöd', 'Högeropposition', 'Högeropposition', 'Högeropposition', 'Högeropposition']
-        färger = ['darkred', 'red', 'darkgreen', 'green', 'deepskyblue', 'blue', 'darkblue', 'yellow']
-        partier = ['V', 'S', 'MP','C', 'L', 'M', 'KD', 'SD']
-        df = pd.read_csv("polls_edit.csv", delimiter=';')
-
-        parti_urval_bool = df.head(1)[partier]>4.0
+        df = OpinionChart.läs_och_preparera_tidsserie_data(False)
+        parti_urval_bool = df.head(1)[GrundData.partier]>GrundData.riksdagsspärr
         
-        partier_urval = list(compress(partier, parti_urval_bool.to_numpy().tolist()[0]))
-        färger_urval = list(compress(färger, parti_urval_bool.to_numpy().tolist()[0]))
-        block_urval = list(compress(block, parti_urval_bool.to_numpy().tolist()[0]))
+        partier_urval = list(compress(GrundData.partier, parti_urval_bool.to_numpy().tolist()[0]))
+        färger_urval = list(compress(GrundData.färger, parti_urval_bool.to_numpy().tolist()[0]))
+        block_urval = list(compress(GrundData.block, parti_urval_bool.to_numpy().tolist()[0]))
 
         df = df.head(1)
         df = df[partier_urval]
         df.index = ['stöd']
         df = df.T
         df.reset_index(inplace=True)
-        df.rename(columns={ df.columns[0]: "parti" }, inplace = True)
-        df['block'] = block_urval
+        df.rename(columns={ df.columns[0]: "Parti" }, inplace = True)
+        df['Block'] = block_urval
         chart = alt.Chart(df).mark_bar().encode(
-            x=alt.X('block:O', scale=alt.Scale(domain=['Regering + stöd', 'Högeropposition'])),
+            x=alt.X('Block:O', scale=alt.Scale(domain=['Regering + stöd', 'Högeropposition'])),
             y = alt.Y('sum(stöd)', title = "Procent"),
-            color = alt.Color('parti:N',
+            color = alt.Color('Parti:N',
                 scale=alt.Scale(domain = partier_urval, range=färger_urval), legend=alt.Legend(orient='right')),
-        ).configure_axisX(
-            labelFontSize=15,
-            labelAngle = 0,
-            titleFontSize=20
+            tooltip = ['Parti:N','stöd']
         )
+     
+        text = alt.Chart(df).mark_text(dx=0,baseline='middle', dy=-8, color='darkslategrey').encode(
+                x=alt.X('Block:O', stack='zero'),
+                y=alt.Y('sum(stöd)'),
+                text=alt.Text('sum(stöd)', format='.1f')
+            )
 
-        return chart
+        return OpinionChart.lägg_till_konfiguration_för_legend_properties_och_axlar(chart + text, 15)
 
 
     @staticmethod
-    def skapa_tids_serie_chart(df_tidsserie, små_partier_bool, genomsnitt_bool):
-        gräns_småparti = 6.0
-
-        partier = ['V', 'S', 'MP','C', 'L', 'M', 'KD', 'SD']
-        färger = ['darkred', 'red', 'darkgreen', 'green', 'deepskyblue', 'blue', 'darkblue', 'yellow']
-
+    def skapa_tidsserie_chart(df_tidsserie, små_partier_bool, genomsnitt_bool):
+        
         if små_partier_bool == False:
             titel = "Opinionssiffror för de större partierna"
-            parti_urval_bool = df_tidsserie.head(1)[partier]>gräns_småparti
+            parti_urval_bool = df_tidsserie.head(1)[GrundData.partier]>GrundData.gräns_småparti
         else:
             titel = "Opinionssiffror för de små partierna"
-            parti_urval_bool = df_tidsserie.head(1)[partier]<gräns_småparti
+            parti_urval_bool = df_tidsserie.head(1)[GrundData.partier]<GrundData.gräns_småparti
         
-        partier_urval = list(compress(partier, parti_urval_bool.to_numpy().tolist()[0]))
-        färger_urval = list(compress(färger, parti_urval_bool.to_numpy().tolist()[0]))
+        partier_urval = list(compress(GrundData.partier, parti_urval_bool.to_numpy().tolist()[0]))
+        färger_urval = list(compress(GrundData.färger, parti_urval_bool.to_numpy().tolist()[0]))
         if genomsnitt_bool:
             y_uttryck = 'mean(stöd):Q'
             datum_utttryck = 'month(Publiceringsdatum)'
@@ -93,8 +87,29 @@ class OpinionChart:
             tool_uttryck = ['Parti:N', y_uttryck, 'Institut']
 
         chart = OpinionChart.lägg_till_encoding_för_chart(chart, datum_utttryck, y_uttryck, partier_urval, färger_urval, tool_uttryck)
-        
-        return OpinionChart.villkorligt_lägg_till_linje_för_spärr(små_partier_bool, chart)
+        chart_och_linje = OpinionChart.villkorligt_lägg_till_linje_för_spärr(små_partier_bool, chart)
+
+        return OpinionChart.lägg_till_konfiguration_för_legend_properties_och_axlar(chart_och_linje)
+
+    @staticmethod
+    def lägg_till_konfiguration_för_legend_properties_och_axlar(chart_uttryck, setlabelFontSize:int = 20):
+        chart_uttryck = chart_uttryck.configure_legend(
+                    strokeColor='gray',
+                    fillColor='#EEEEEE',
+                    padding=10,
+                    cornerRadius=10,
+                    orient='top-right',
+                    title = None,
+                    labelFontSize = 14,
+                    symbolStrokeWidth = 8).properties(
+                            width=420,
+                            height=340).configure_axis(
+                            labelFontSize=setlabelFontSize,
+                            labelAngle = 0,
+                            titleFontSize=20
+                    )
+        return chart_uttryck
+
     
     @staticmethod
     def lägg_till_encoding_för_chart(chart, datum_utttryck, y_uttryck, partier_urval, färger_urval, tool_uttryck):
@@ -151,7 +166,6 @@ st.set_page_config(
 # grunddata navigering
 användar_val = ['de små partierna', 'de större partierna', 'de två blocken']
 
-
 # navigering
 left, mid, right = st.beta_columns(3)
 with left:
@@ -166,30 +180,14 @@ visa_månadsgenomsnitt = st.checkbox("Visa snittvärden för tidsserier", value=
 df = OpinionChart.läs_och_preparera_tidsserie_data()
 
 if ((__vilket_val() == 0) or (__vilket_val() == 1)):
-    chart_u = OpinionChart.skapa_tids_serie_chart(df, (__vilket_val() == 0), visa_månadsgenomsnitt)
+    chart_u = OpinionChart.skapa_tidsserie_chart(df, (__vilket_val() == 0), visa_månadsgenomsnitt)
 else:
     chart_u = OpinionChart.skapa_block_chart()
-    st.write("Partierna under riksdagsspärren räknas inte med. I nuläget är det ett parti i vardera block som ligger under spärren i opinionsundersökningarna.")
+    st.write("Partierna under riksdagsspärren räknas inte med.")
     
-
 
 st.altair_chart(
     chart_u
-    .configure_legend(
-        strokeColor='gray',
-        fillColor='#EEEEEE',
-        padding=10,
-        cornerRadius=10,
-        orient='top-right',
-        title = None,
-        labelFontSize = 14,
-        symbolStrokeWidth = 8)
-    .properties(
-            width=420,
-            height=340).configure_axis(
-            labelFontSize=20,
-            titleFontSize=20
-    ) 
 )
 
 with st.beta_expander("Data referenser"):
